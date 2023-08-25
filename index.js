b81115bc631b0599c2f9a3ce7ced9195844b156e
Jian Raphael R. Cudiamat <2212288@slu.edu.ph>
2023-08-25 18:56:52 +0800

    BUGS TO FIX: * user redirect after logging in to index page * user should not be able to return to log in page after logging in

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const port = 3000;
-
-mongoose.connect('mongodb://127.0.0.1/intellijenttesting', {useNewUrlParser: true, useUnifiedTopology: true});
-console.log("Successfully Connected to the database!");
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+var name = null;
+
+// Connect to the database
+mongoose.connect('mongodb://127.0.0.1/intellijent', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(session({
   secret: 'your secret here',
@@ -18,88 +18,128 @@ app.use(session({
   saveUninitialized: true
 }));
 
+function checkLoggedIn(req, res, next) {
+    if (req.session.loggedIn) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//IMAGINE THIS TO BE A METHOD GRABBING USER CREDS FROM MONGOOSE
-var name = "Jiannis Cudiamititis";
-//-----------------
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    emp_id: Number,
+    first_name: String,
+    last_name: String
+  });
+
+const User = mongoose.model('User', userSchema);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get('/', function(req, res){
-        res.render('index', {
-            title: 'Home page'
-        });
-        console.log();
+app.get('/', checkLoggedIn, function(req, res){
+    if (req.session.loggedIn) {
+        res.redirect('/'); // if logged in
+    } else {
+        res.redirect('login'); // if not logged in
+    }
+        // res.render('index', {
+        //     title: 'Home page'
+        // });
+        // console.log(req.session.first_name);
 });
 
 app.get('/logout', function(req, res){
-        res.redirect('login');
-        console.log("User has logged out!");
+    req.session.destroy();
+    res.redirect('/login');
+    console.log("User has logged out!");
 });
 
 app.get('/login', function(req, res){
+    if (req.session.loggedIn) {
+        res.redirect('/index');
+    } else {
         res.render('login', {
-            title: 'Login Page', name: name
+            title: 'Login Page'
         });
+    }
 });
 
-
-const userSchema = new mongoose.Schema({
-          username: String,
-          password: String,
-          first_name: String,
-          last_name: String
-        });
-
-const User = mongoose.model('User', userSchema);
-
 app.post('/login', function (req, res) {
-    var username = req.body.userName;
-    var password = req.body.passWord;
+    if(req.session.loggedIn){
+        res.render('index', {
+            title: 'Home Page', first_name: req.session.first_name, last_name: req.session.last_name
+        });
+    } else {
+        var username = req.body.userName;
+        var password = req.body.passWord;
 
-    User.findOne({ username: username })
+        User.findOne({ username: username }) // finds the inputted username
         .then(function(user) {
             if (!user) {
-                console.log("failure to find user");
+                console.log("Failed to find User");
                 res.render('login', {
-                    title: 'Login Page', receivedError: "Incorrect Username or Password!"
+                    title: 'Login Page', receivedError: "Wrong User Credentials!"
                 });
-            } else if (password === user.password) {
+            } else if (password === user.password) { // if correct password
+
+                req.session.loggedIn = true;
+                req.session.username = user.username;
+                req.session.emp_id = user.emp_id;
+                req.session.first_name = user.first_name;
+                req.session.last_name = user.last_name;                
+
                 res.render('index', {
                     title: 'Home Page', name: user.username
                 });
-                console.log("correct user password OMG");
+                console.log("User " + user.first_name, user.last_name, user.emp_id + " has logged in.");
             } else {
                 res.render('login', {
-                    title: 'Login Page', name: "Wrong Answer Beech"
+                    title: 'Login Page', name: "Wrong User Credentials"
                 });
-                console.log("wrong everything shet");
+                console.log("Wrong User Credentials!");
             }
         })
         .catch(function(error) {
             console.error(error);
             res.status(500).send('Internal Server Error');
         });
+    }
 });
 
 app.get('/createform', function(req, res){
+    if(req.session.loggedIn) {
+        res.redirect('/createform');
+    } else {
         res.render('createform', {
             title: 'CREATE FORM', name: name
         });
+    }
 });
 
 app.get('/createuser', function(req, res){
+
+    if (req.session.loggedIn) {
+        res.redirect('/createuser');
+    } else {
         res.render('createuser', {
             title: 'CREATE USER', name: name
         });
+    }
 });
 
 app.get('/viewform', function(req, res){
+    if(req.session.loggedIn) {
+        res.redirect('/viewform');
+    } else {
         res.render('viewform', {
             title: 'View Forms', name: name
         });
+    }        
 });
