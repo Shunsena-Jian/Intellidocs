b24ca49be4de36eb5bd54f008366276f77e014be
2143007@slu.edu.ph <brightlight1>
2023-11-12 03:24:13 +0800

    *formview.pug //Added save form modal +handle_ajax_save_filled_out_form.js //Added modal script when saving form

--- views/js/client.functions/handle_ajax_save_filled_out_form.js
+++ views/js/client.functions/handle_ajax_save_filled_out_form.js
@@ -1,3 +1,25 @@
+var saveFormModal = document.getElementById('saveFormModal');
+var saveFormModalOverLay = document.getElementById('saveFormModalOverLay');
+var saveFormModalDialogbox = document.getElementById('saveFormModalDialogbox');
+
+function showSaveFormModal() {
+    saveFormModal.style.display = 'block';
+
+    saveFormModalOverLay.style.display = 'block';
+    saveFormModalOverLay.style.height = 2000+"px";
+
+    saveFormModalDialogbox.style.top = "100px";
+    saveFormModalDialogbox.style.display = "block";
+    saveFormModalDialogbox.style.position = "fixed";
+}
+
+function hideSaveFormModal(){
+    saveFormModal.style.display = 'none';
+    saveFormModalOverLay.style.display = "none";
+    saveFormModalDialogbox.style.display = "none";
+}
+
+
 function saveFilledOutForm(){
     var formBody = document.getElementById('theContainerOfTheForm').innerHTML;
 
@@ -14,6 +36,7 @@ function saveFilledOutForm(){
                 alert("Error in saving the form.");
             } else if (response.status_code === 0) {
                 alert("Successfully saved the form.");
+                hideSaveFormModal();
             } else {
                 $('.error-message').text(response.error);
             }
